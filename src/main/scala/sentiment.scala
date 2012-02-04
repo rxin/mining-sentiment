44ef6fb2e6a1f5f74391f36c549c9d65bf5220c9
Reynold Xin <reynoldx@gmail.com>
2012-02-03 17:28:22 -0800

    Print a table of results for different parameter values. Parameters include stemming and multinomial/Bernoulli.

--- src/main/scala/sentiment.scala
+++ src/main/scala/sentiment.scala
@@ -1,7 +1,6 @@
 package sentiment
 
-import scalanlp.text.tokenize._
-
+import scalanlp.text.tokenize._ 
 import scala.collection.JavaConversions._
 import scala.io.Source
 
@@ -9,9 +8,10 @@ import java.io.File
 
 import scalala.tensor.mutable._
 
-import scala.math.log
+import scala.math._
 import scala.util.Random
 import scalanlp.data._
+
 
 object Sentiment {
 
@@ -21,6 +21,8 @@ object Sentiment {
   }
   import SentimentLabel._
 
+  case class Parameters(stemmer: String => String, alpha: Double, binaryCounts: Boolean)
+
   // TODO: index words
 
   type Word = String
@@ -28,17 +30,17 @@ object Sentiment {
 
   val masterRandom = Random
 
-  case class Model(counts: Counter2[SentimentLabel, Word, Double],
-                   labelCounts : Counter[SentimentLabel, Double],
-                   alpha: Double) {
+  case class Model(parameters: Parameters) {
     val wordSet = new scala.collection.mutable.HashSet[Word]
+    val counts = Counter2[SentimentLabel, Word, Double]
+    val labelCounts = Counter[SentimentLabel, Double]
 
     def score(label: SentimentLabel, document: Document): Double = {
       var score: Double = 0
-      score += log(labelCounts(label) + alpha)
-      score -= document.length * log((labelCounts(label) + wordSet.size * alpha))
+      score += log(labelCounts(label) + parameters.alpha)
+      score -= document.length * log((labelCounts(label) + wordSet.size * parameters.alpha))
       for (word <- document) {
-        score += log(counts(label, word) + alpha)
+        score += log(counts(label, word) + parameters.alpha)
       }
       return score
     }
@@ -50,8 +52,10 @@ object Sentiment {
     }
 
     def trainOne(label: SentimentLabel, words: Document) {
-      wordSet ++= words
-      for (word <- words) {
+      val stemWords = words.map(parameters.stemmer)
+      val maybeUniqWords = if (parameters.binaryCounts) stemWords.distinct else stemWords
+      wordSet ++= maybeUniqWords
+      for (word <- maybeUniqWords) {
         counts(label, word) += 1
         labelCounts(label) += 1
       }
@@ -69,15 +73,17 @@ object Sentiment {
     }
   }
 
-  def train(trainingData: Seq[LabeledDocument[SentimentLabel, Word]]) = {
-    val model = Model(Counter2(), Counter(), 1)
+  def train(
+    parameters: Parameters,
+    trainingData: Seq[LabeledDocument[SentimentLabel, Word]]) = {
+    val model = Model(parameters)
     for (train <- trainingData) model.trainOne(train.label, train.features("body"))
     model
   }
 
   def f1Score(model: Model, testData: Seq[LabeledDocument[SentimentLabel, Word]],
               label: SentimentLabel): Double = {
-    val counts = testData.map { doc =>
+    val counts = (testData.map { doc =>
       val docLabel = model.classify(doc.features("body"))
       if (docLabel == doc.label) {
         if (doc.label == label) {
@@ -92,7 +98,10 @@ object Sentiment {
           "fn"
         }
       }
-    }.groupBy(x => x) map { case (key, value) => (key, value size) }
+    } ++ List("fp", "fn", "tp", "tn")).groupBy(x => x) map {
+      case (key, value) => (key, value.size - 1)
+    }
+
     val betaSq: Double = 1
     return (1 + betaSq) * counts("tp") /
            ((1 + betaSq) * counts("tp") + betaSq * counts("fn") + counts("fp"))
@@ -110,22 +119,35 @@ object Sentiment {
   }
 
   def main(args: Array[String]) {
+
     val neg = getLabeledDocuments(new File(dataPath, "neg").listFiles, Neg)
     val pos = getLabeledDocuments(new File(dataPath, "pos").listFiles, Pos)
-
     val all = masterRandom.shuffle(neg ++ pos)
+
+    val alphas =
+      List(1.0) ++
+      collection.immutable.Range.Double(log(0.1), log(3), log(2)/2).map(x => exp(x))
     val sliceSize = all.size / 10
-    val scores = (0 until 10).par map { i => {
-      val a = i * sliceSize
-      val b = (i + 1) * sliceSize
-      val testData = all.slice(a, b)
-      val trainingData = all.take(a) ++ all.drop(b)
-      val model = train(trainingData)
-      val score = evaluate(model, testData)
-      println(score)
-      score
+   
+    List(false, true).map { binaryCount => {
+      List(false, true).map { stem => {
+        alphas.map { alpha => {
+          val stemmer = if (stem) PorterStemmer else ((x : String) => x)
+          val parameters = Parameters(stemmer, alpha, binaryCount)
+          val scores = (0 until 10).par map { i => {
+            val a = i * sliceSize
+            val b = (i + 1) * sliceSize
+            val testData = all.slice(a, b)
+            val trainingData = all.take(a) ++ all.drop(b)
+            val model = train(parameters, trainingData)
+            evaluate(model, testData)
+          }}
+          val result = (binaryCount, stem, alpha, scores.sum / scores.size)
+          println(result)
+          result
+        }}
+      }}
     }}
-    println("Congratulations!  Your average score is " + scores.sum / scores.size)
   }
 
 }
